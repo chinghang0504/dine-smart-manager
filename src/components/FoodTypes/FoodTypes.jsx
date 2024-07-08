e61a37c9f036b5629d4e373306e07925c35a16b9
Ching Hang Lam <chinghang0504@gmail.com>
2024-07-07 20:04:39 -0700

    Update the menu of food types

--- src/components/FoodTypes/FoodTypes.jsx
+++ src/components/FoodTypes/FoodTypes.jsx
@@ -1,33 +1,37 @@
 import { useEffect, useState } from 'react';
 import './FoodTypes.scss';
 import { getAllFoodTypes, deleteFoodType } from '../../services/server';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function FoodTypes() {
   const [foodTypes, setFoodTypes] = useState([]);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    async function loadData() {
-      const foodTypes = await getAllFoodTypes();
-      setFoodTypes(foodTypes);
-    }
-    loadData();
-  }, []);
+  // Load the data
+  async function loadData() {
+    const foodTypes = await getAllFoodTypes();
+    setFoodTypes(foodTypes);
+  }
 
+  // Click the delete button
   async function clickDeleteButton(id) {
     try {
       await deleteFoodType(id);
-      const foodTypes = await getAllFoodTypes();
-      setFoodTypes(foodTypes);
+      loadData();
     } catch {
-      console.log('Error');
+      // No action
     }
   }
 
+  // Click the edit button
   function clickEditButton(id) {
     navigate(`/menu/editfoodtype?id=${id}`);
   }
+
+  // Execute once
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
     <div className="food-types">
@@ -35,7 +39,7 @@ function FoodTypes() {
         <ul className='food-types-header__list'>
           <li className='food-types-header__item'>ID</li>
           <li className='food-types-header__item'>Type</li>
-          <li className='food-types-header__item'>Image</li>
+          <li className='food-types-header__item food-types-header__item--image'>Image</li>
           <li className='food-types-header__item'>Priority</li>
           <li className='food-types-header__item'>Actions</li>
         </ul>
@@ -44,29 +48,27 @@ function FoodTypes() {
         return (
           <div className='food-types-type' key={index}>
             <div className='food-types-type__content-container'>
-              <h3>ID</h3>
-              <p>{foodType.id}</p>
+              <p className='food-types-type__title'>ID:</p>
+              <p className='food-types-type__description'>{foodType.id}</p>
             </div>
-            <div>
-              <div className='food-types-type__content-container'>
-                <h3>Type</h3>
-                <p>{foodType.type}</p>
-              </div>
-              <div className='food-types-type__content-container'>
-                <h3>Priority</h3>
-                <p>{foodType.priority}</p>
-              </div>
+            <div className=' food-types-type__content-container'>
+              <p className='food-types-type__title'>Type:</p>
+              <p className='food-types-type__description'>{foodType.type}</p>
             </div>
-            <div className='food-types-type__content-container'>
-              <h3>Image</h3>
+            <div className='food-types-type__content-container food-types-type__content-container--image'>
+              <p className='food-types-type__title'>Image:</p>
               <img className='food-types-type__image' src={`${import.meta.env.VITE_SERVER_URL}/foodtypes/${foodType.image}`} alt={foodType.type} />
-              <p>{foodType.image}</p>
+              <p className='food-types-type__description'>{foodType.image}</p>
             </div>
             <div className='food-types-type__content-container'>
-              <h3>Actions</h3>
-              <div>
-                <button onClick={() => clickEditButton(foodType.id)}><i className="bi bi-pencil"></i></button>
-                <button onClick={() => clickDeleteButton(foodType.id)}><i className="bi bi-trash"></i></button>
+              <p className='food-types-type__title'>Priority:</p>
+              <p className='food-types-type__description'>{foodType.priority}</p>
+            </div>
+            <div className='food-types-type__content-container'>
+              <p className='food-types-type__title'>Actions:</p>
+              <div className='food-types-type__action-container'>
+                <button className="food-types-type__button food-types-type__button--edit" onClick={() => clickEditButton(foodType.id)}><i className="bi bi-pencil"></i> Edit</button>
+                <button className="food-types-type__button food-types-type__button--delete" onClick={() => clickDeleteButton(foodType.id)}><i className="bi bi-pencil"></i> Delete</button>
               </div>
             </div>
           </div>
