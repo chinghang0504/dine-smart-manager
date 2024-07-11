acab2b04baa65c9f39290d3bb74eaafde7693121
Ching Hang Lam <chinghang0504@gmail.com>
2024-07-11 12:50:32 -0700

    Add socket io client

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,8 +1,24 @@
 import './Orders.scss';
+import { useEffect, useState } from 'react';
+import socketIOClient from 'socket.io-client';
+
+const ENDPOINT = 'http://127.0.0.1:5000'; //endpoint port 5000
 
 function Orders() {
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on('FromAPI', (data) => {
+      console.log(data);
+      // setResponse(data);
+    });
+
+  }, []);
+  
+
   return (
-    <div>Orders</div>
+    <p>{response}</p>
   )
 }
 
