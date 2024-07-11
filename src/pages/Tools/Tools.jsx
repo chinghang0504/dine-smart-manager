656e3601fb4472e5bdb69e87776afccaa729d5da
Ching Hang Lam <chinghang0504@gmail.com>
2024-07-11 04:34:10 -0700

    Add two buttons to the QR Code Generator

--- src/pages/Tools/Tools.jsx
+++ src/pages/Tools/Tools.jsx
@@ -6,11 +6,39 @@ function Tools() {
   const baseUrl = "https://google.com/menu/";
   const tableNum = 1;
   const [url, setUrl] = useState(`${baseUrl}${tableNum}`);
-  const baseUrlRef = useRef();
-  const tableNumRef = useRef();
+  const baseUrlRef = useRef(null);
+  const tableNumRef = useRef(null);
+  const qrCodeRef = useRef(null);
 
+  // Change the base url or table number inputs
   function changeInput() {
+    if (!tableNumRef.current.value) {
+      tableNumRef.current.value = '1';
+    }
     setUrl(`${baseUrlRef.current.value}${tableNumRef.current.value}`);
+  }
+
+  // Save the QR code
+  function saveQr() {
+    const canvas = qrCodeRef.current.querySelector('canvas');
+    const link = document.createElement('a');
+    link.href = canvas.toDataURL('image/png');
+    link.download = `qrcode_table_${tableNumRef.current.value}.png`;
+    link.click();
+  }
+
+  // Print the QR code
+  function printQr() {
+    const canvas = qrCodeRef.current.querySelector('canvas');
+    const printWindow = window.open('', '_blank');
+    printWindow.document.write(`
+      <html>
+        <body>
+          <img src="${canvas.toDataURL('image/png')}" onload="window.print();" />
+        </body>
+      </html>
+    `);
+    printWindow.document.close();
   }
 
   return (
@@ -23,7 +51,14 @@ function Tools() {
         <input className='qr-code-generator__input' type='text' id='base-url' defaultValue={baseUrl} ref={baseUrlRef} onChange={changeInput} />
         <label className='qr-code-generator__label' htmlFor="table-num">Table Number</label>
         <input className='qr-code-generator__input qr-code-generator__input--table-num' type="number" id='table-num' min="1" defaultValue={tableNum} ref={tableNumRef} onChange={changeInput} />
-        <QrCodeGenerator url={url} />
+        <div ref={qrCodeRef}>
+          <QrCodeGenerator url={url} />
+        </div>
+
+      </div>
+      <div className='tools-action-container'>
+        <button className='tools-action-container__button' onClick={saveQr}>Save</button>
+        <button className='tools-action-container__button' onClick={printQr}>Print</button>
       </div>
     </div>
 
